9253dde9af12910d5e5cc696793f397f23f081ae
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-05-08 17:54:24 +0200

    add initial setup copy

---
+++ 01-react-fundamentals--initial-setup/.eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'array-bracket-spacing': 'error',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'key-spacing': ['error', { 'mode': 'strict', 'beforeColon': false, 'afterColon': true }],
+    'react-refresh/only-export-components': 'warn',
+    'semi': 'error',
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+  },
+};
---
+++ 01-react-fundamentals--initial-setup/README.md
@@ -0,0 +1,10 @@
+# 01 React Fundamentals: Initial Setup
+
+This repository is intended to follow along the React Fundamentals lecture series.
+
+Please make sure to do the following:
+1. Install Node.js ([link](https://nodejs.org/en)) - The lecture uses v18.16.0 (LTS at time of recording), but any version should be fine
+2. Run `npm install`
+3. (Optional) If using Visual Studio Code, installing the ESLint extension ([link](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)) is advised.
+
+To start the app just run `npm run dev`
