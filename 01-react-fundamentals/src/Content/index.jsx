3ae101c56fb474911ad8dd46ac6e13528e884fd7
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-05-09 17:19:10 +0200

    [checkpoint] after update

--- 01-react-fundamentals/src/Content/index.jsx
+++ 01-react-fundamentals/src/Content/index.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 import Cell from './Cell';
 
 function Content() {
   const [totalCount, setTotalCount] = useState(0);
-  const handleClick = () => {
+  const handleClick = useCallback(() => {
     setTotalCount((oldValue) => oldValue + 1);
-  };
+  }, []);
+
+  const handleClick2 = useMemo(() => {
+    return () => {
+      setTotalCount((oldValue) => oldValue + 1);
+    };
+  }, []);
+
+  const style = useMemo(() => {
+    return { color: 'white', textAlign: 'center' };
+  }, []);
 
   console.log('render Content');
   return (
-    <div className="content" style={{ color: 'white', textAlign: 'center' }}>
-      <Cell title="A" color="#F3A712" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="B" color="#FF0054" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="C" color="#3772FF" totalCount={totalCount} onClick={handleClick} />
-      <Cell title="D" color="#BAA898" totalCount={totalCount} onClick={handleClick} />
+    <div className="content" style={style}>
+      <Cell title="A" color="#F3A712" totalCount={'N/A'} onClick={handleClick} />
+      <Cell title="B" color="#FF0054" totalCount={'N/A'} onClick={handleClick} />
+      <Cell title="C" color="#3772FF" totalCount={'N/A'} onClick={handleClick} />
+      <Cell title="D" color="#BAA898" totalCount={'N/A'} onClick={handleClick} />
     </div>
   );
 }
--- 01-react-fundamentals/src/main.jsx
+++ 01-react-fundamentals/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
@@ -6,8 +6,11 @@ import App from './App';
 import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
