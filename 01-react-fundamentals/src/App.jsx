3b93890bab4288a90e04d81114efd3d0a9deef63
pero5arNotch <petar.kovacevic@wearenotch.com>
2023-05-09 15:02:04 +0200

    [checkpoint] after state

--- 01-react-fundamentals/src/App.jsx
+++ 01-react-fundamentals/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import logo from './assets/logo.png';
 
 function Header() {
@@ -17,17 +18,30 @@ function Header() {
 }
 
 function Cell(props) {
-  const { title, color } = props;
+  const { title, color, totalCount, onClick } = props;
   const style = { backgroundColor: color, padding: '5%', height: '10em' };
 
+  const [counter, setCounter] = useState({ value: 0 });
+  const handleClick = () => {
+    setCounter({ ...counter, value: counter.value + 1 });
+    // setCounter((oldValue) => { value: });
+    onClick();
+  };
+
   return (
-    <div style={style}>
+    <div style={style} onClick={handleClick}>
       <h2>{title}</h2>
+      <span>{counter.value} / {totalCount}</span>
     </div>
   );
 }
 
 function App() {
+  const [totalCount, setTotalCount] = useState(0);
+  const handleClick = () => {
+    setTotalCount((oldValue) => oldValue + 1);
+  };
+
   return (
     <div className="app">
       <div style={{ backgroundColor: '#51D6A9', margin: 10, padding: "10px" }}>
@@ -35,10 +49,10 @@ function App() {
         <Header />
 
         <div className="content" style={{ color: 'white', textAlign: 'center' }}>
-          <Cell title="A" color="#F3A712" />
-          <Cell title="B" color="#FF0054" />
-          <Cell title="C" color="#3772FF" />
-          <Cell title="D" color="#BAA898" />
+          <Cell title="A" color="#F3A712" totalCount={totalCount} onClick={handleClick} />
+          <Cell title="B" color="#FF0054" totalCount={totalCount} onClick={handleClick} />
+          <Cell title="C" color="#3772FF" totalCount={totalCount} onClick={handleClick} />
+          <Cell title="D" color="#BAA898" totalCount={totalCount} onClick={handleClick} />
         </div>
 
       </div>
